e58187dc2810cb3351f29301892ed3adffc4c319
stephane brossier <stephane@ning.com>
2013-05-15 11:40:42 -0700

    First gen for all killbill objects

--- lib/payment_test/api.rb
+++ lib/payment_test/api.rb
@@ -19,19 +19,19 @@ module PaymentTest
     end
 
     def process_payment(kb_account_id, kb_payment_id, kb_payment_method_id, amount_in_cents, currency, options = {})
-      Killbill::Plugin::Gen::PaymentInfoPlugin.new(amount_in_cents, DateTime.now, DateTime.now, Killbill::Plugin::PaymentStatus::SUCCESS, "gateway_error", "gateway_error_code", nil, nil)
+      Killbill::Plugin::Gen::PaymentInfoPlugin.new(amount_in_cents, DateTime.now, DateTime.now, Killbill::Plugin::Gen::PaymentPluginStatus::PROCESSED, "gateway_error", "gateway_error_code", nil, nil)
     end
 
     def get_payment_info(kb_account_id, kb_payment_id, options = {})
-        Killbill::Plugin::Gen::PaymentInfoPlugin.new(0, DateTime.now, DateTime.now, Killbill::Plugin::PaymentStatus::SUCCESS, "gateway_error", "gateway_error_code", nil, nil)
+        Killbill::Plugin::Gen::PaymentInfoPlugin.new(0, DateTime.now, DateTime.now, Killbill::Plugin::Gen::PaymentPluginStatus::PROCESSED, "gateway_error", "gateway_error_code", nil, nil)
     end
 
     def process_refund(kb_account_id, kb_payment_id, amount_in_cents, currency, options = {})
-      Killbill::Plugin::Gen::RefundInfoPlugin.new(amount_in_cents, DateTime.now, DateTime.now, Killbill::Plugin::PaymentStatus::SUCCESS, "gateway_error", "gateway_error_code", nil)
+      Killbill::Plugin::Gen::RefundInfoPlugin.new(amount_in_cents, DateTime.now, DateTime.now, Killbill::Plugin::Gen::RefundPluginStatus::PROCESSED, "gateway_error", "gateway_error_code", nil)
     end
     
     def get_refund_info(kb_account_id, kb_payment_id, options = {})
-      Killbill::Plugin::Gen::RefundInfoPlugin.new(amount_in_cents, DateTime.now, DateTime.now, Killbill::Plugin::PaymentStatus::SUCCESS, "gateway_error", "gateway_error_code", nil)
+      Killbill::Plugin::Gen::RefundInfoPlugin.new(amount_in_cents, DateTime.now, DateTime.now, illbill::Plugin::Gen::RefundPluginStatus::PROCESSED, "gateway_error", "gateway_error_code", nil)
     end
 
     def add_payment_method(kb_account_id, kb_payment_method_id, payment_method_props, set_default, options = {})
@@ -42,7 +42,7 @@ module PaymentTest
     end
 
     def get_payment_method_detail(kb_account_id, kb_payment_method_id, options = {})
-        Killbill::Plugin::Gen::RefundInfoPlugin.PaymentMethodPlugin.new("foo", true, [], nil, "type", "cc_name", "cc_type", "cc_expiration_month", "cc_expiration_year", "cc_last4", "address1", "address2", "city", "state", "zip", "country")
+        Killbill::Plugin::Gen::PaymentMethodPlugin.new("foo", true, [], nil, "type", "cc_name", "cc_type", "cc_expiration_month", "cc_expiration_year", "cc_last4", "address1", "address2", "city", "state", "zip", "country")
     end
 
     def set_default_payment_method(kb_account_id, kb_payment_method_id, options = {})
